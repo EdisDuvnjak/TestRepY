c0fb1dbb80c3bfa1d93a74849a42cdad0bb158f8
EdisDuvnjak <79976423+EdisDuvnjak@users.noreply.github.com>
2021-03-24 19:08:18 +0100

    Summary

---
+++ ConsoleAppY/ConsoleAppY/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Halo Bola");
+            Console.ReadKey();
+        }
+    }
+}
